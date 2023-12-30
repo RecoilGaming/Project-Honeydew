c9adc8551f93f9ceda2328b918ce5ef5135788a5
RecoilGaming <qinzhaolee@gmail.com>
2023-12-29 20:42:48 -0800

    Random push

--- Project Honeydew/Assets/Scripts/UI/Healthbar.cs
+++ Project Honeydew/Assets/Scripts/UI/Healthbar.cs
@@ -1,13 +1,7 @@
-<<<<<<< Updated upstream
-using System.Collections;
-=======
->>>>>>> Stashed changes
 using UnityEngine;
 
 public class Healthbar : MonoBehaviour
 {
-<<<<<<< Updated upstream
-    // private variables
     [SerializeField] private Transform healthOverlay;
 
     public void SetHealthPercent(float health)
@@ -15,12 +9,5 @@ public class Healthbar : MonoBehaviour
         Vector2 scale = healthOverlay.localScale;
         scale.x = health;
         healthOverlay.localScale = scale;
-=======
-    public void SetHealthPercent(float health)
-    {
-        Vector2 scale = transform.localScale;
-        scale.x = health;
-        transform.localScale = scale;
->>>>>>> Stashed changes
     }
 }
